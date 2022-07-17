f53860c821b3f337adf47ff2b4dc0e3cc3830b00
carys <carys@gmail.com>
2022-07-17 19:03:36 +0100

    addded more to models and database

--- holidays/models.py
+++ holidays/models.py
@@ -1,4 +1,5 @@
 # from random import choice, choices
+from pyexpat import model
 from django.db import models
 
 from django import forms
@@ -8,21 +9,17 @@ from django import forms
 class Holiday(models.Model):
     HotelName = models.CharField(max_length =100)
     Area = models.CharField(max_length =100)
-    Continent = models.CharField(max_length =100)
-    Country = models.CharField(max_length =100)
-    Category = models.CharField(max_length =100)
     StarRating = models.CharField(max_length =100)
-    TempRating = models.CharField(max_length =100)
     Location = models.CharField(max_length =100)
     PricePerPerNight = models.CharField(max_length =100)
     
-
     def __str__(self):
         return(f"{self.HotelName}, {self.Area},{self.Continent}, {self.Country}, {self.Category}, {self.StarRating},{self.TempRating}, {self.Location}, {self.PricePerPerNight}")
 
 
 class Continent(models.Model):
     continents_field = models.CharField(max_length =100)
+    
 
     def __str__(self):
         return self.continents_field
@@ -30,25 +27,28 @@ class Continent(models.Model):
 
 class AfricaCountries(models.Model):
     countries = models.CharField(max_length =100)
+    continent = models.ManyToManyField(Continent)
 
     def __str__(self):
         return self.countries
 
 class AustraliaCountries(models.Model):
     countries = models.CharField(max_length =100)
-
+    continent = models.ManyToManyField(Continent)
 
     def __str__(self):
         return self.countries
 
 class AntarcticaCountries(models.Model):
     countries = models.CharField(max_length =100)
+    continent = models.ManyToManyField(Continent)
 
     def __str__(self):
         return self.countries
 
 class NorthAmericaCountries(models.Model):
     countries = models.CharField(max_length =100)
+    continent = models.ManyToManyField(Continent)
 
     def __str__(self):
         return self.countries
@@ -56,6 +56,7 @@ class NorthAmericaCountries(models.Model):
 
 class EuropeCountries(models.Model):
     countries = models.CharField(max_length =100)
+    continent = models.ManyToManyField(Continent)
     
 
     def __str__(self):
@@ -64,14 +65,25 @@ class EuropeCountries(models.Model):
 
 class AsiaCountries(models.Model):
     countries = models.CharField(max_length =100)
+    continent = models.ManyToManyField(Continent)
 
     def __str__(self):
         return self.countries
 
 class TypeHoliday(models.Model):
     type_hol = models.CharField(max_length =100)
-    # holiday = models.ManyToManyField(Holiday)
-    continent = models.ManyToManyField(Holiday)
+    holiday_details = models.ManyToManyField(Holiday)
+
     def __str__(self):
-        return (f'types is hoildays {self.continent} ')
- 
+        return (f'types is hoildays {self.type_hol} ')
+
+class Temp(models.Model):
+    holiday = models.ManyToManyField(Holiday)
+    temp = models.CharField(max_length =100)
+
+    def __str__(self):
+        return (f'types is hoildays {self.temp} ')
+
+# class Result(models.Model):
+#     result = models.ManyToManyField(Holiday related_name='holidays')
+#     type = models.ManyToManyField(Holiday related_name='person2persons')
