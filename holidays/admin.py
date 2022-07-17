3c3d871c3c9b57253965b15ea92235ef11d060eb
carys <carys@gmail.com>
2022-07-18 00:09:38 +0100

    files working added more to models

--- holidays/admin.py
+++ holidays/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Holiday, Continent, AsiaCountries, AfricaCountries, NorthAmericaCountries, EuropeCountries, AntarcticaCountries, AustraliaCountries, TypeHoliday, Temp, Result
-admin.site.register(Holiday)
+from .models import  Continent, AsiaCountries, AfricaCountries, NorthAmericaCountries, EuropeCountries, AntarcticaCountries, AustraliaCountries, TypeHol
+# admin.site.register(Holiday)
 admin.site.register(Continent)
 admin.site.register(AfricaCountries)
 admin.site.register(AsiaCountries)
@@ -9,9 +9,9 @@ admin.site.register(NorthAmericaCountries)
 admin.site.register(EuropeCountries)
 admin.site.register(AntarcticaCountries)
 admin.site.register(AustraliaCountries)
-admin.site.register(TypeHoliday)
-admin.site.register(Temp)
-admin.site.register(Result)
+admin.site.register(TypeHol)
+
+# admin.site.register(Result)
 # admin.site.register(
 # admin.site.register(
 # admin.site.register(
--- holidays/models.py
+++ holidays/models.py
@@ -5,26 +5,11 @@ from django.db import models
 
 from django import forms
 
-
-
-class Holiday(models.Model):
-    HotelName = models.CharField(max_length =100)
-    Area = models.CharField(max_length =100)
-    StarRating = models.CharField(max_length =100)
-    Location = models.CharField(max_length =100)
-    PricePerPerNight = models.CharField(max_length =100)
-    continents_field = models.CharField(max_length =100)
-    
-    def __str__(self):
-        return(f"{self.HotelName}, {self.Area}, {self.Location}, {self.PricePerPerNight}")
-
-
 class Continent(models.Model):
     continents_field = models.CharField(max_length =100)
     
     def __str__(self):
         return self.continents_field
-
 
 class AfricaCountries(models.Model):
     continent = models.ManyToManyField(Continent)
@@ -63,8 +48,6 @@ class EuropeCountries(models.Model):
     continent = models.ManyToManyField(Continent)
     countries = models.CharField(max_length =100)
 
-    
-
     def __str__(self):
         return self.countries
 
@@ -76,24 +59,23 @@ class AsiaCountries(models.Model):
     def __str__(self):
         return (F'Countries are : {self.countries} and continent are {self.continent}')
 
-class TypeHoliday(models.Model):
+class TypeHol(models.Model):
     type_hol = models.CharField(max_length =100)
+    temp = models.CharField(max_length =100)
 
     def __str__(self):
         return (f'types is hoildays {self.type_hol} ')
 
-class Temp(models.Model):
-    temp = models.CharField(max_length =100)
 
 
-class Result(models.Model):
-    result = models.ManyToManyField(Holiday)
+class Holiday(models.Model):
+    continent = models.ManyToManyField(Continent)
+    # type = models.ManyToManyField(TypeHol)
+    HotelName = models.CharField(max_length =100)
+    Area = models.CharField(max_length =100)
+    StarRating = models.CharField(max_length =100)
+    Location = models.CharField(max_length =100)
+    PricePerPerNight = models.CharField(max_length =100)
+    
     def __str__(self):
-        return (f'types is hoildays {self.result} ')
-
-
-# # class Type(models.Models):
-
-# class Result(models.Model):
-#     result = models.ManyToManyField(Holiday)
-#     type = models.ManyToManyField(TypeHoliday)
+        return(f"{self.HotelName}, {self.Area}, {self.Location}, {self.PricePerPerNight}")
